ceaa705f5a51285aae0d64f810584ea64be984b7
jon Rittenberg <2460887+jritt@users.noreply.github.com>
2025-01-28 16:56:30 -0500

    adding the kitchen sink!

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+	// site: 'https://carverbryce.com',
+	site: 'https://jritt.github.io/carverbryce',
+  // base: 'carverbryce',
+  integrations: [sitemap()]
+/* 	build: {
+    inlineStylesheets: 'always',
+  }, */
+
+	//Changes dist/_astro to dist/______
+	//build: {  assets: 'custom-folder' },
+});
+
