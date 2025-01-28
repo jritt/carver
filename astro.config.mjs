8c2a33e537dd1fd1ac4768b3748d05e0a1ce6c49
jon Rittenberg <2460887+jritt@users.noreply.github.com>
2025-01-28 17:10:22 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
 	// site: 'https://carverbryce.com',
-	site: 'https://jritt.github.io/carverbryce',
-  // base: 'carverbryce',
+	site: 'https://jritt.github.io/carver',
+  base: 'dist',
   integrations: [sitemap()]
 /* 	build: {
     inlineStylesheets: 'always',
