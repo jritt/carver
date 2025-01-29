a890e3d27ebf889082d2ce3673202d35a2f3c822
jon Rittenberg <2460887+jritt@users.noreply.github.com>
2025-01-28 21:08:00 -0500

    just dist files at root!

--- xxxx/astro.config.mjs
+++ xxxx/astro.config.mjs
@@ -4,8 +4,8 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
 	// site: 'https://carverbryce.com',
-	site: 'https://jritt.github.io',
-  base: '/carver',
+	site: 'https://jritt.github.io/carver',
+  // base: '/carver',
   integrations: [sitemap()]
 /* 	build: {
     inlineStylesheets: 'always',
